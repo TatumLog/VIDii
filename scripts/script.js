0bd53f7df3241a65e0ad8c98d77b4215bacd3ad0
TatumLog <131144209+TatumLog@users.noreply.github.com>
2023-04-24 15:36:11 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
 // JavaScript Document
 console.log("Howdy!");
+
+
+const audioPlayer = document.getElementById('audioPlayer');
+const raceauto = document.querySelector('.afbeelding2');
+
+// Speel het MP3-bestand af wanneer er over de raceauto wordt gehoverd
+raceauto.addEventListener('mouseover', () => {
+    // console.log("over");
+    audioPlayer.play();
+});
+
+// Pauzeer het MP3-bestand wanneer de hover over de raceauto wordt verwijderd
+raceauto.addEventListener('mouseout', () => {
+    // console.log("out");
+    audioPlayer.pause();
+});	
